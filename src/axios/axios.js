a141ed0e44f18d44204262770b00d6639826069b
AfsalKabeerU <afsalkabeer4u@gmail.com>
2025-08-31 22:59:37 +0530

    Update axios.js

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://nh-foods-erp-dev-backend.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
